a476cd769ab76695f812c59cb0c2147c8585a0a4
Noémi Salaün <noemi@beemoov.com>
2019-01-07 15:57:38 +0100

    dev(5): basic and fully optimized solution

--- problem-5.php
+++ problem-5.php
@@ -17,6 +17,71 @@ declare(strict_types=1);
  */
 function problem5(int $limit = 20): int
 {
-    // TODO
-    return 2520;
+    $result = 1;
+
+    for ($i = 1; $i <= $limit; $i++) {
+
+        // Incremental search based on the previous result found.
+        $increment = $result;
+
+        while ($result % $i !== 0) {
+            $result += $increment;
+        }
+    }
+
+    return $result;
 }
+
+/**
+ * @deprecated First solution, archived.
+ *
+ * @param int $limit
+ *
+ * @return int
+ */
+function oldProblem5(int $limit = 20): int
+{
+    $increment = 1;
+
+    // Iterativly search for lower solution and use the previous result to optimize the next search.
+    for ($lowerLimit = 1; $lowerLimit <= $limit; $lowerLimit++) {
+        $increment = doProblem5($lowerLimit, $increment);
+    }
+
+    return $increment;
+}
+
+/**
+ * Use an optimized $increment value to search for the solution.
+ *
+ * @param int $limit
+ * @param int $increment
+ *
+ * @return int
+ */
+function doProblem5(int $limit, int $increment): int
+{
+
+    // No need to test sub-multiples.
+    $threshold = $limit / 2;
+
+    $n = 0;
+    do {
+        $n += $increment;
+
+        $ok = true;
+
+        // We increment by a multiple of $limit each time, so no need to test $limit.
+        // Stop to $threshold because ne need to test sub-multiples.
+        for ($i = $limit - 1; $i > $threshold; $i--) {
+            if ($n % $i !== 0) {
+                $ok = false;
+                break;
+            }
+        }
+
+    } while (!$ok);
+
+    return $n;
+}
+
